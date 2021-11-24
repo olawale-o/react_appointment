00915d65a79cb58bcedc7bcea58dbe39230d14ba
olawale-o <omoogunolawale@gmail.com>
2021-11-24 08:20:10 +0100

    Add login page

---
+++ src/components/authentication/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './login.module.css';
+
+const Login = () => (
+  <div className={style.loginContainer}>
+    <div className={style.form}>
+      <form>
+        <h3>Book an appointment</h3>
+        <div className={style.field}>
+          <input type="text" placeholder="Username" className={style.input} required />
+        </div>
+        <div className="actions">
+          <input type="submit" value="Login" className={style.loginBtn} />
+        </div>
+      </form>
+    </div>
+  </div>
+);
+
+export default Login;
