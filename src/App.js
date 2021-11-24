c6857078bf28977e40e42f9289d9a16ae4d5ce7c
olawale-o <omoogunolawale@gmail.com>
2021-11-24 16:31:16 +0100

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/authentication/Login';
+import Home from './components/home/Home';
+import List from './components/doctor/List';
 
 const App = () => (
   <div className="App">
     <Routes>
-      <Route path="/" element={<Login />} exact />
+      <Route path="/" element={<Home />}>
+        <Route path="/" element={<List />} />
+        <Route path="add-a-doctor" element={<h1>Add a doctor</h1>} />
+      </Route>
       <Route path="/login" element={<Login />} exact />
     </Routes>
   </div>
