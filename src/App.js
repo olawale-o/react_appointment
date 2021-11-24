c48715610d001c6b4adb354d3f5db6898efae618
olawale-o <omoogunolawale@gmail.com>
2021-11-24 08:36:46 +0100

    Use react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/authentication/Login';
 
-const App = () => <div className="App"><Login /></div>;
+const App = () => (
+  <div className="App">
+    <Routes>
+      <Route path="/" element={<Login />} exact />
+      <Route path="/login" element={<Login />} exact />
+    </Routes>
+  </div>
+);
 export default App;
