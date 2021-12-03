b6736a5e9b28c73c4dc8a4e011a3f7a9dc3c1b95
olawale-o <omoogunolawale@gmail.com>
2021-12-03 16:30:02 +0100

    User can login

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import style from './login.module.css';
+import { login } from '../../redux/auth/auth_async_actions';
+import authSelector from '../../redux/auth/auth_selector';
 
-const Login = () => (
-  <div className={style.loginContainer}>
-    <div className={style.form}>
-      <form>
-        <h3>Book an appointment</h3>
-        <div className={style.field}>
-          <input type="text" placeholder="Username" className={style.input} required />
-        </div>
-        <div className={style.field}>
-          <input type="password" placeholder="Password" className={style.input} required />
-        </div>
-        <div className="actions">
-          <input type="submit" value="Login" className={style.loginBtn} />
-        </div>
-      </form>
+const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isLoading, user } = useSelector(authSelector);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, [user]);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const credentials = {
+      user: { email, password },
+    };
+    dispatch(login(credentials));
+  };
+  return (
+    <div className={style.loginContainer}>
+      <div className={style.form}>
+        <form onSubmit={handleFormSubmit}>
+          <h3>Book an appointment</h3>
+          <div className={style.field}>
+            <input
+              type="text"
+              placeholder="Username"
+              className={style.input}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className={style.field}>
+            <input
+              type="password"
+              placeholder="Password"
+              className={style.input}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="actions">
+            {isLoading
+              ? <p>Loading</p>
+              : <input type="submit" value="Login" className={style.loginBtn} />}
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Login;
