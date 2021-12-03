4ebe06ca7b22ab02964fe42c78b4ed6b10fdcd2b
olawale-o <omoogunolawale@gmail.com>
2021-12-03 16:29:06 +0100

    Add error key to state

--- src/redux/auth/index.js
+++ src/redux/auth/index.js
@@ -1,16 +1,19 @@
-import { AUTH_LOADING, AUTH_SUCCESS } from "./action_types";
+import { AUTH_LOADING, AUTH_SUCCESS, AUTH_FAIL } from './action_types';
 
 const initialState = {
   isLoading: false,
   user: null,
+  error: null,
 };
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case AUTH_SUCCESS:
-      return {  ...state, user: action.payload, isLoading: false };
+      return { ...state, user: action.payload, isLoading: false };
     case AUTH_LOADING:
       return { ...state, isLoading: true };
+    case AUTH_FAIL:
+      return { ...state, error: action.payload, isLoading: false };
     default:
       return state;
   }
