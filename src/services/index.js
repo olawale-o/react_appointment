7bde95461140fd7161bf29da714df84f750c1f7e
olawale-o <omoogunolawale@gmail.com>
2021-12-03 10:43:57 +0100

    Create login service

---
+++ src/services/index.js
@@ -0,0 +1,7 @@
+import post from '../api';
+const BASE_URI = 'http://localhost:3000/v1';
+
+export const login = (credentials) => {
+  const response = await post(`${BASE_URI}/users/login`, credentials);
+  return response.json();
+};
