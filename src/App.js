21f834f76225b8490997871135eeb17af010d7ed
olawale-o <omoogunolawale@gmail.com>
2021-12-03 15:40:51 +0100

    Use PrivateRoute component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/authentication/Login';
+import Detail from './components/doctor/Detail';
+import PrivateRoute from './components/routes/PrivateRoute';
 
 const App = () => (
   <div className="App">
     <Routes>
-      <Route path="/" element={<Login />} exact />
       <Route path="/login" element={<Login />} exact />
+      <Route
+        path="/"
+        element={
+          (
+            <PrivateRoute>
+              <Detail />
+            </PrivateRoute>
+          )
+        }
+        exact
+      />
     </Routes>
   </div>
 );
