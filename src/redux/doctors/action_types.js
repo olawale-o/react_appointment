46180c79787a917915cb0b5c009903d0e7a89792
olawale-o <omoogunolawale@gmail.com>
2021-12-04 09:57:57 +0100

    Create action types for doctors

---
+++ src/redux/doctors/action_types.js
@@ -0,0 +1,4 @@
+export const REQUEST_LOADING = 'doctors/loading';
+export const ALL_DOCTORS = 'doctors/allDoctors';
+export const SINGLE_DOCTOR = 'doctors/singleDoctor';
+export const REQUEST_FAILURE = 'doctors/failure'
