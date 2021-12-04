3a1b6f117d34ce30cf5aca5ce8f63c8dd2167691
olawale-o <omoogunolawale@gmail.com>
2021-12-04 09:58:19 +0100

    Create action creators for doctors

---
+++ src/redux/doctors/action_creators.js
@@ -0,0 +1,11 @@
+import {
+  ALL_DOCTORS,
+  REQUEST_FAILURE,
+  REQUEST_LOADING,
+  SINGLE_DOCTOR
+} from './action_types';
+
+export const loading = () => ({ type: REQUEST_LOADING });
+export const allDoctors = (payload) => ({ type: ALL_DOCTORS, payload });
+export const singleDoctor = (payload) => ({ type: SINGLE_DOCTOR, payload });
+export const error = (payload) => ({ type: REQUEST_FAILURE, payload });
