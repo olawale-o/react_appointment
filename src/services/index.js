ed4b25ded6a355e6c699bef256c3f978823dd14a
olawale-o <omoogunolawale@gmail.com>
2021-12-04 10:02:02 +0100

    Create service to get all doctors

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,4 @@
-import post from '../api';
+import { post, get } from '../api';
 
 const BASE_URI = 'http://localhost:3000/v1';
 
@@ -9,5 +9,10 @@ export const loginService = async (credentials) => {
 
 export const registerService = async (credentials) => {
   const response = await post(`${BASE_URI}/users`, credentials);
+  return response.json();
+};
+
+export const getDoctorsService = async (credentials) => {
+  const response = await get(`${BASE_URI}/doctors`);
   return response.json();
 };
