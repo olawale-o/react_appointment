c3be34ff211360d23434cc748e32b4ee331a3a58
olawale-o <omoogunolawale@gmail.com>
2021-12-05 15:33:39 +0100

    Add async functions for doctors

---
+++ src/redux/doctors/doctor_async_action.js
@@ -0,0 +1,34 @@
+import { getDoctorsService, getDoctorService } from '../../services';
+import {
+  ALL_DOCTORS,
+  REQUEST_FAILURE,
+  REQUEST_LOADING,
+  SINGLE_DOCTOR
+} from './action_types';
+
+export const getDoctors = () => (
+  async function (dispatch) {
+    dispatch({ type: REQUEST_LOADING });
+    try {
+      const response = await getDoctorsService();
+      dispatch({ type: ALL_DOCTORS, payload: response });
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: REQUEST_FAILURE, payload: error });
+    }
+  }
+);
+
+export const getDoctor = (id) => (
+  async function (dispatch) {
+    dispatch({ type: REQUEST_LOADING });
+    try {
+      const response = await getDoctorService(id);
+      dispatch({ type: SINGLE_DOCTOR, payload: response });
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: REQUEST_FAILURE, payload: error });
+    }
+  }
+);
+  
