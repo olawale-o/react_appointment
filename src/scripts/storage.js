d654275120b42ab210648d9ffee66032c0e9e679
olawale-o <omoogunolawale@gmail.com>
2021-12-06 06:23:29 +0100

    Create helper function to add and retrieve from localStorage

---
+++ src/scripts/storage.js
@@ -0,0 +1,5 @@
+export const setStorage = (value, key = 'appointment') => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const getStorage = (key = 'appointment') => JSON.parse(localStorage.getItem(key));
