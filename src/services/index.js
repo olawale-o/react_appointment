6344ad6f02f98b030084e092962ffec31850af3a
olawale-o <omoogunolawale@gmail.com>
2021-12-06 06:53:14 +0100

    Add set token on login or register service

--- src/services/index.js
+++ src/services/index.js
@@ -1,14 +1,17 @@
 import { post, get } from '../api';
+import { setStorage } from '../scripts/storage';
 
 const BASE_URI = 'http://localhost:3000/v1';
 
 export const loginService = async (credentials) => {
   const response = await post(`${BASE_URI}/users/login`, credentials);
+  setStorage(response.headers.get('Authorization').split(' ')[1], 'token');
   return response.json();
 };
 
 export const registerService = async (credentials) => {
   const response = await post(`${BASE_URI}/users`, credentials);
+  setStorage(response.headers.get('Authorization').split(' ')[1], 'token');
   return response.json();
 };
 
