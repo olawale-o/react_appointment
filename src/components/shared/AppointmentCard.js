e66c3270664ae1d8b01a26029cf0121576748fea
olawale-o <omoogunolawale@gmail.com>
2021-12-08 15:18:48 +0100

    Create AppointmentCard component

---
+++ src/components/shared/AppointmentCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+const AppointmentCard = ({ appointment }) => (
+  <>
+    <div className="appointment__item">
+      <div className="appointment__info">
+        <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" style={{ width: '40px', height: '40px', borderRadius: '50%' }} alt="" />
+        <span className="fs-3">{appointment.fullname}</span>
+      </div>
+      <div className="appointment__city">
+        <span className="fs-3">{appointment.city}</span>
+      </div>
+      <div className="appointment__specialty">
+        <span className="fs-3">{appointment.specialty}</span>
+      </div>
+      <div className="appointment__date">
+        <span className="fs-3">2020-02-08</span>
+      </div>
+      <span className="fs-6">
+        <i className="bx bxs-trash-alt" />
+      </span>
+    </div>
+    <div className="divider" />
+  </>
+);
+
+export default AppointmentCard;
+
+AppointmentCard.propTypes = {
+  appointment: PropType.shape({
+    id: PropType.number,
+    fullname: PropType.string,
+    specialty: PropType.string,
+    city: PropType.string,
+    appointment_date: PropType.string,
+  }).isRequired,
+};
