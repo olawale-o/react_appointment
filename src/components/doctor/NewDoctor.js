e1526f97934f7b06ca34a75a64323201a02cf533
olawale-o <omoogunolawale@gmail.com>
2021-12-08 10:36:59 +0100

    Fix conflict error

--- src/components/doctor/NewDoctor.js
+++ src/components/doctor/NewDoctor.js
@@ -3,36 +3,6 @@ import { useNavigate } from 'react-router';
 import { useDispatch } from 'react-redux';
 import { addDoctor } from '../../redux/doctors/doctor_async_action';
 
-<<<<<<< HEAD
-const NewDoctor = () => (
-  <div className="doctor__new">
-    <div className="form">
-      <form>
-        <h1 className="heading1 mb-2" style={{ color: '#fff' }}>ADD A NEW DOCTOR</h1>
-        <div className="field">
-          <input type="text" placeholder="Doctor's Name" className="input" required />
-        </div>
-        <div className="field">
-          <select required>
-            <option>Select a city</option>
-            <option value="1">Lagos</option>
-            <option value="2">Toronto</option>
-          </select>
-        </div>
-        <div className="field">
-          <input type="text" placeholder="Specialization" className="input" required />
-        </div>
-        <div className="field">
-          <textarea placeholder="Description" rows="5" cols="35" className="textarea" required />
-        </div>
-        <div className="field">
-          <input type="file" className="input" required />
-        </div>
-        <div className="action">
-          <input type="submit" value="Add Doctor" className="btn addBtn" />
-        </div>
-      </form>
-=======
 const NewDoctor = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -111,7 +81,6 @@ const NewDoctor = () => {
           </div>
         </form>
       </div>
->>>>>>> develop
     </div>
   );
 };
