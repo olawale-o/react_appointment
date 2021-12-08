1d6f12c25eb268e763f505376f6f8323a7d0ad42
olawale-o <omoogunolawale@gmail.com>
2021-12-08 10:08:44 +0100

    Unpack data object

--- src/redux/doctors/doctor_async_action.js
+++ src/redux/doctors/doctor_async_action.js
@@ -1,4 +1,8 @@
-import { getDoctorsService, getDoctorService } from '../../services';
+import {
+  getDoctorsService,
+  getDoctorService,
+  addDoctorService,
+} from '../../services';
 import {
   ALL_DOCTORS,
   REQUEST_FAILURE,
@@ -23,7 +27,20 @@ export const getDoctor = (id) => (
     dispatch({ type: REQUEST_LOADING });
     try {
       const response = await getDoctorService(id);
-      dispatch({ type: SINGLE_DOCTOR, payload: response });
+      dispatch({ type: SINGLE_DOCTOR, payload: response.data });
+    } catch (error) {
+      dispatch({ type: REQUEST_FAILURE, payload: error });
+    }
+  }
+);
+
+export const addDoctor = (newDoctor, cb) => (
+  async function addDoctor(dispatch) {
+    dispatch({ type: REQUEST_LOADING });
+    try {
+      const { data: { doctor } } = await addDoctorService(newDoctor);
+      dispatch({ type: SINGLE_DOCTOR, payload: doctor });
+      cb(doctor.id);
     } catch (error) {
       dispatch({ type: REQUEST_FAILURE, payload: error });
     }
