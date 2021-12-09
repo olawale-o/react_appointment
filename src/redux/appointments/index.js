bb1aa984c9d8e26f82c4f91d785b976b371f3f97
olawale-o <omoogunolawale@gmail.com>
2021-12-09 09:07:18 +0100

    Create appointmentReducer

---
+++ src/redux/appointments/index.js
@@ -0,0 +1,30 @@
+import {
+  ALL_APPOINTMENTS,
+  REQUEST_FAILURE,
+  REQUEST_LOADING,
+  REQUEST_SUCCESS
+} from './action_types';
+
+const initialState = {
+  appoinntments: [],
+  loading: false,
+  error: null,
+  message: null,  
+};
+
+const appointmentReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ALL_APPOINTMENTS:
+      return {  ...state, appoinntments: action.payload, loading: false };
+    case REQUEST_LOADING:
+      return { ...state, loading: true };
+    case REQUEST_FAILURE:
+      return { ...state, error: action.payload, loading: false };
+    case REQUEST_SUCCESS:
+      return { ...state, message: action.payload, loading: false };
+    default:
+      return state;
+  }
+};
+
+export default appointmentReducer;
