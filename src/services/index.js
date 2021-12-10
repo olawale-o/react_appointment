dc799850ba3d07fb67027573b0382da295b57324
olawale-o <omoogunolawale@gmail.com>
2021-12-10 14:18:07 +0100

    Replace long args with object

--- src/services/index.js
+++ src/services/index.js
@@ -4,13 +4,13 @@ import { getStorage, setStorage } from '../scripts/storage';
 const BASE_URI = 'http://localhost:3000/v1';
 
 export const loginService = async (credentials) => {
-  const response = await post(`${BASE_URI}/users/login`, credentials);
+  const response = await post(`${BASE_URI}/users/login`, { credentials });
   setStorage(response.headers.get('Authorization').split(' ')[1], 'token');
   return response.json();
 };
 
 export const registerService = async (credentials) => {
-  const response = await post(`${BASE_URI}/users`, credentials);
+  const response = await post(`${BASE_URI}/users`, { credentials });
   setStorage(response.headers.get('Authorization').split(' ')[1], 'token');
   return response.json();
 };
@@ -27,13 +27,15 @@ export const getDoctorService = async (id) => {
 
 export const addDoctorService = async (doctor) => {
   const token = getStorage('token');
-  const response = await post(`${BASE_URI}/doctors`, doctor, token, true);
+  const options = { credentials: doctor, token, picture: true };
+  const response = await post(`${BASE_URI}/doctors`, options);
   return response.json();
 };
 
 export const createAppointmentService = async (appointment) => {
   const token = getStorage('token');
-  const response = await post(`${BASE_URI}/appointments`, appointment, token);
+  const options = { token, credentials: appointment };
+  const response = await post(`${BASE_URI}/appointments`, options);
   return response.json();
 };
 
