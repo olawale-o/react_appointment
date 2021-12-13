c3ac640e594ec20eee44819837fc4bb09a113b10
olawale-o <omoogunolawale@gmail.com>
2021-12-13 08:31:11 +0100

    Create logout modal

---
+++ src/components/shared/LogoutModal.js
@@ -0,0 +1,41 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import PropType from 'prop-types';
+import authSelector from '../../redux/auth/auth_selector';
+import { logout } from '../../redux/auth/auth_async_actions';
+import styles from './LogoutModalStyle';
+
+const LogoutModal = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+  const dispatch = useDispatch();
+  const { isLoading } = useSelector(authSelector);
+  const onConfirm = () => {
+    dispatch(logout());
+  };
+  return (
+    ReactDOM.createPortal(
+      <div className="modal" style={styles.modal}>
+        <div className="modal__body" style={styles.modalBody}>
+          <h6 className="modal__header" style={styles.modalHeader}>Are you sure</h6>
+          <div className="modal__actions" style={styles.modalActions}>
+            <button type="button" style={styles.cancel} onClick={onClose}>
+              Cancel
+            </button>
+            {isLoading
+              ? <div className="form__submission-indicator" />
+              : <button type="button" style={styles.confirm} onClick={onConfirm}>Logout</button>}
+          </div>
+        </div>
+      </div>,
+      document.body,
+    )
+  );
+};
+
+export default LogoutModal;
+
+LogoutModal.propTypes = {
+  isOpen: PropType.bool.isRequired,
+  onClose: PropType.func.isRequired,
+};
