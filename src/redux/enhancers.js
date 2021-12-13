2702dd7aa30c67716d7c219ceff1508e30e4f74c
olawale-o <omoogunolawale@gmail.com>
2021-12-13 04:08:04 +0100

    Customize logger

--- src/redux/enhancers.js
+++ src/redux/enhancers.js
@@ -1,8 +1,11 @@
 import { applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import logger from 'redux-logger';
+import logger from './logger';
 
-const middlewares = [thunkMiddleware, logger];
+const middlewares = [thunkMiddleware];
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 const enhancers = applyMiddleware(...middlewares);
 const composedEnhancers = compose(enhancers);
 
