25cb2bd71f5438bd96ac7c736bdc301bcda25073
olawale-o <omoogunolawale@gmail.com>
2021-12-13 03:30:59 +0100

    Fix token retrieve

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,5 @@
 import { getStorage } from '../scripts/storage';
 
-const token = getStorage('token');
-
 export const post = async (url, options = {}) => {
   const { credentials = {}, token = '', picture = false } = options;
   const headers = {
@@ -22,6 +20,7 @@ export const post = async (url, options = {}) => {
 };
 
 export const get = async (url) => {
+  const token = getStorage('token');
   const headers = {
     headers: {
       'Content-type': 'application/json; Charset=UTF-8',
@@ -36,6 +35,7 @@ export const get = async (url) => {
 };
 
 export const remove = async (url) => {
+  const token = getStorage('token');
   const headers = {
     headers: {
       'Content-type': 'application/json; Charset=UTF-8',
