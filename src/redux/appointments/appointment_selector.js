cb816f228bcf9e9736cc097f96bff3e1d0064236
olawale-o <omoogunolawale@gmail.com>
2021-12-22 10:23:58 +0100

    Create selectors for appointments state

--- src/redux/appointments/appointment_selector.js
+++ src/redux/appointments/appointment_selector.js
@@ -1,3 +1,4 @@
-const appointmentSelector = (state) => state.appointments;
-
-export default appointmentSelector;
+export const selectAppointments = (state) => state.appointments.allAppointmentIds;
+export const selectAppointmentsById = (id) => (state) => state.appointments.appointmentById[id];
+export const selectAppointmentsLoading = (state) => state.appointments.loading;
+export const selectAppointmentsMessage = (state) => state.appointments.message;
