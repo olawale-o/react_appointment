fb02abdb7915e53e38e826daa4a1d40b21ec014a
olawale-o <omoogunolawale@gmail.com>
2021-12-22 08:43:36 +0100

    Create selectors for doctors state

--- src/redux/doctors/doctor_selector.js
+++ src/redux/doctors/doctor_selector.js
@@ -1,3 +1,3 @@
-const doctorSelector = (state) => state.doctors;
-
-export default doctorSelector;
+export const selectDoctors = (state) => state.doctors.allDoctorIds;
+export const selectDoctorById = (id) => (state) => state.doctors.doctorById[id];
+export const selectLoading = (state) => state.doctors.loading;
