57e27ace324874d0f131e46c01d6bae7e2591acf
olawale-o <omoogunolawale@gmail.com>
2021-12-22 10:22:15 +0100

    Create normalizedAppointments schema

--- src/redux/schema/index.js
+++ src/redux/schema/index.js
@@ -3,10 +3,17 @@ import { normalize, schema } from 'normalizr';
 const doctorSchema = new schema.Entity('doctors');
 const doctorListSchema = [doctorSchema];
 
-const normalizedDoctors = (data) => {
+const appointmentSchema = new schema.Entity('appointments');
+const appointmentListSchema = [appointmentSchema];
+
+export const normalizedDoctors = (data) => {
   const doctorList = data.doctors;
   const { entities: { doctors }, result } = normalize(doctorList, doctorListSchema);
   return { allDoctorIds: result, doctorById: doctors };
 };
 
-export default normalizedDoctors;
+export const normalizedAppointments = (data) => {
+  const appointmentList = data.appointments;
+  const { entities: { appointments }, result } = normalize(appointmentList, appointmentListSchema);
+  return { allAppointmentIds: result, appointmentById: appointments };
+};
