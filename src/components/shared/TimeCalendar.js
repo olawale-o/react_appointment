2bbae7b7985152237140027f8218dc1f50b8b90e
olawale-o <omoogunolawale@gmail.com>
2022-09-30 03:46:36 +0100

    Create TimeCalendar component

---
+++ src/components/shared/TimeCalendar.js
@@ -0,0 +1,59 @@
+import PropType from 'prop-types';
+import ButtonDate from './ButtonDate';
+
+const TimeCalendar = ({
+  monthName,
+  fullDate,
+  days,
+  prevDate,
+  nextDate,
+}) => (
+  <div className="calendar">
+    <div className="month">
+      <button type="button" className="btn-direction prev" onClick={prevDate}>
+        <i className="bx bx-chevron-left icon" />
+      </button>
+      <div className="date">
+        <h1 className="month__name">{monthName}</h1>
+        <p className="month__full">{fullDate}</p>
+      </div>
+      <button type="button" className="btn-direction next" onClick={nextDate}>
+        <i className="bx bxs-chevron-right icon" />
+      </button>
+    </div>
+    <div className="weekdays">
+      <div className="weekday">Sun</div>
+      <div className="weekday">Mon</div>
+      <div className="weekday">Tue</div>
+      <div className="weekday">Wed</div>
+      <div className="weekday">Thu</div>
+      <div className="weekday">Fri</div>
+      <div className="weekday">Sat</div>
+    </div>
+    <div className="days">
+      {days.map((day) => (
+        <ButtonDate
+          key={day.key}
+          date={day.date}
+          prevDate={day.prevDate}
+          nextDate={day.nextDate}
+        />
+      ))}
+    </div>
+  </div>
+);
+
+export default TimeCalendar;
+
+TimeCalendar.propTypes = {
+  monthName: PropType.string.isRequired,
+  fullDate: PropType.string.isRequired,
+  days: PropType.arrayOf(PropType.shape({
+    date: PropType.number.isRequired,
+    prevDate: PropType.bool.isRequired,
+    nextDate: PropType.bool.isRequired,
+    key: PropType.string.isRequired,
+  })).isRequired,
+  prevDate: PropType.func.isRequired,
+  nextDate: PropType.func.isRequired,
+};
