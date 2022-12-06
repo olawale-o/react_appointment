00ffeacde65e723be5d570b5c732ef91fc0b7b39
olawale-o <omoogunolawale@gmail.com>
2022-12-06 12:41:42 +0100

    Add slider images

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,9 @@
 import MONTHS from './months';
 import showDays from './showDays';
+import sliderImages from './sliderImages';
 
 export {
   MONTHS,
   showDays,
+  sliderImages,
 };
---
+++ src/constants/sliderImages.js
@@ -0,0 +1,13 @@
+import doctorOne from '../assets/doctor-one.jpg';
+import doctorTwo from '../assets/doctor-two.jpg';
+
+const sliderImages = [
+  {
+    image: doctorOne,
+  },
+  {
+    image: doctorTwo,
+  },
+];
+
+export default sliderImages;
