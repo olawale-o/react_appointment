6e35e3a603225ab4ea8bac7773e7b82fe656539a
olawale-o <omoogunolawale@gmail.com>
2024-02-13 12:44:42 +0100

    Toggle valid dates on calendar

--- src/components/shared/CalendarDays.jsx
+++ src/components/shared/CalendarDays.jsx
@@ -1,6 +1,6 @@
 import PropType from 'prop-types';
 
-import { format, isPast } from 'date-fns';
+import { format, isPast, isToday } from 'date-fns';
 // import { bookings } from './data';
 // import style from './TimeCalendar.module.css';
 
@@ -12,7 +12,7 @@ const CalendarDays = ({ days }) => (
       <li
         key={day}
       >
-        <button type="button" className="date-button" disabled={isPast(day)}>
+        <button type="button" className="date-button" disabled={isPast(day) && !isToday(day)}>
           {format(day, 'd')}
         </button>
       </li>
